118a4c16a73cde07fa6f2381cc77cae855259c49
Nugine <nugine@foxmail.com>
2022-03-07 15:21:42 +0800

    update benches

--- benches/maps.rs
+++ benches/maps.rs
@@ -2,98 +2,104 @@ use ordered_vecmap::OrderedVecMap;
 
 use std::collections::{BTreeMap, HashMap};
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use rand::Rng;
 
 pub fn get_trivial(c: &mut Criterion) {
-    let n = 1024 * 1024 * 16;
+    let mut group = c.benchmark_group("get_trivial");
+    for n in [3, 5, 16, 1024, 4 * 1024, 16 * 1024] {
+        let mut rng = rand::thread_rng();
+        let data = {
+            let mut v: Vec<u64> = vec![0; n];
+            rng.fill(&mut *v);
+            v
+        };
 
-    let mut rng = rand::thread_rng();
-    let data = {
-        let mut v: Vec<u32> = vec![0; n];
-        rng.fill(&mut *v);
-        v
-    };
+        let input = data.first().copied().unwrap();
 
-    let input = data.first().copied().unwrap();
+        {
+            let map = data
+                .iter()
+                .map(|&x| (x, x.to_string()))
+                .collect::<OrderedVecMap<_, _>>();
 
-    {
-        let map = data
-            .iter()
-            .map(|&x| (x, x.to_string()))
-            .collect::<OrderedVecMap<_, _>>();
+            let id = BenchmarkId::new("ordered-vecmap", n);
+            group.bench_with_input(id, &input, |b, &i| {
+                b.iter(|| black_box(map.get(black_box(&i)).unwrap()));
+            });
+        }
 
-        c.bench_function("ordered-vecmap_get_trivial", |b| {
-            b.iter(|| black_box(map.get(black_box(&input)).unwrap()));
-        });
-    }
+        {
+            let map = data
+                .iter()
+                .map(|&x| (x, x.to_string()))
+                .collect::<BTreeMap<_, _>>();
 
-    {
-        let map = data
-            .iter()
-            .map(|&x| (x, x.to_string()))
-            .collect::<BTreeMap<_, _>>();
+            let id = BenchmarkId::new("btreemap", n);
+            group.bench_with_input(id, &input, |b, i| {
+                b.iter(|| black_box(map.get(black_box(i)).unwrap()));
+            });
+        }
 
-        c.bench_function("btreemap_get_trivial", |b| {
-            b.iter(|| black_box(map.get(black_box(&input)).unwrap()));
-        });
-    }
+        {
+            let map = data
+                .iter()
+                .map(|&x| (x, x.to_string()))
+                .collect::<HashMap<_, _>>();
 
-    {
-        let map = data
-            .iter()
-            .map(|&x| (x, x.to_string()))
-            .collect::<HashMap<_, _>>();
-
-        c.bench_function("hashmap_get_trivial", |b| {
-            b.iter(|| black_box(map.get(black_box(&input)).unwrap()));
-        });
+            let id = BenchmarkId::new("hashmap", n);
+            group.bench_with_input(id, &input, |b, i| {
+                b.iter(|| black_box(map.get(black_box(i)).unwrap()));
+            });
+        }
     }
 }
 
 pub fn get_nontrivial(c: &mut Criterion) {
-    let n = 1024 * 128;
+    let mut group = c.benchmark_group("get_nontrivial");
+    for n in [16, 1024, 4 * 1024, 16 * 1024] {
+        let mut rng = rand::thread_rng();
+        let data = {
+            let mut v: Vec<u64> = vec![0; n];
+            rng.fill(&mut *v);
+            v
+        };
 
-    let mut rng = rand::thread_rng();
-    let data = {
-        let mut v: Vec<u32> = vec![0; n];
-        rng.fill(&mut *v);
-        v
-    };
+        let input = data.first().copied().unwrap().to_string();
 
-    let input = data.first().copied().unwrap().to_string();
+        {
+            let map = data
+                .iter()
+                .map(|&x| (x.to_string(), x.to_string()))
+                .collect::<OrderedVecMap<_, _>>();
 
-    {
-        let map = data
-            .iter()
-            .map(|&x| (x.to_string(), x.to_string()))
-            .collect::<OrderedVecMap<_, _>>();
+            let id = BenchmarkId::new("ordered-vecmap", n);
+            group.bench_with_input(id, &input, |b, i| {
+                b.iter(|| black_box(map.get(black_box(i)).unwrap()));
+            });
+        }
 
-        c.bench_function("ordered-vecmap_get_nontrivial", |b| {
-            b.iter(|| black_box(map.get(black_box(&input)).unwrap()));
-        });
-    }
+        {
+            let map = data
+                .iter()
+                .map(|&x| (x.to_string(), x.to_string()))
+                .collect::<BTreeMap<_, _>>();
+            let id = BenchmarkId::new("btreemap", n);
+            group.bench_with_input(id, &input, |b, i| {
+                b.iter(|| black_box(map.get(black_box(i)).unwrap()));
+            });
+        }
 
-    {
-        let map = data
-            .iter()
-            .map(|&x| (x.to_string(), x.to_string()))
-            .collect::<BTreeMap<_, _>>();
-
-        c.bench_function("btreemap_get_nontrivial", |b| {
-            b.iter(|| black_box(map.get(black_box(&input)).unwrap()));
-        });
-    }
-
-    {
-        let map = data
-            .iter()
-            .map(|&x| (x.to_string(), x.to_string()))
-            .collect::<HashMap<_, _>>();
-
-        c.bench_function("hashmap_get_nontrivial", |b| {
-            b.iter(|| black_box(map.get(black_box(&input)).unwrap()));
-        });
+        {
+            let map = data
+                .iter()
+                .map(|&x| (x.to_string(), x.to_string()))
+                .collect::<HashMap<_, _>>();
+            let id = BenchmarkId::new("hashmap", n);
+            group.bench_with_input(id, &input, |b, i| {
+                b.iter(|| black_box(map.get(black_box(i)).unwrap()));
+            });
+        }
     }
 }
 
